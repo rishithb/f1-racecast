7e943058aefd769d570561d9f85a932912dba334
Rishith Bandi <bandirishith@gmail.com>
2025-03-01 20:59:28 -0600

    telemetry replay data tested and working from fast-f1 api

--- frontend/racecast/src/components/TestData.js
+++ frontend/racecast/src/components/TestData.js
@@ -1,11 +1,13 @@
-// filepath: /Users/rishithbandi/repos/f1-racecast/frontend/racecast/src/components/CarData.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const TestData = () => {
-  const [data, setData] = useState(null);
+  const [carData, setCarData] = useState(null);
+  const [fastData, setFastData] = useState(null);
+  const [latestTelemetry, setLatestTelemetry] = useState({ Time: '', Speed: '' });
 
   useEffect(() => {
+    console.log('Making request to backend...');
     axios.get('http://127.0.0.1:8000/car_data', {
       params: {
         driver_number: 55,
@@ -14,21 +16,53 @@ const TestData = () => {
       }
     })
     .then(response => {
-      setData(response.data);
+      setCarData(response.data);
     })
     .catch(error => {
       console.error('There was an error fetching the data!', error);
     });
   }, []);
 
+  useEffect(() => {
+    console.log('Getting FastF1 data...');
+    axios.get('http://127.0.0.1:8000/max')
+    .then(response2 => {
+      console.log('FastF1 response received: ', response2.data);
+      setFastData(response2.data);
+    })
+    .catch(error => {
+      console.error('There was an error fetching the FastF1 data!', error);
+    });
+  }, []);
+
+  useEffect(() => {
+    const eventSource = new EventSource("http://127.0.0.1:8000/stream");
+
+    eventSource.onmessage = (event) => {
+      const newData = JSON.parse(event.data);
+      setLatestTelemetry(newData);
+      console.log('New telemetry data received: ', newData);
+    };
+
+    eventSource.onerror = (error) => {
+      console.error("EventSource failed:", error);
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
     <div>
       <h1>Test Data</h1>
-      {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : (
-        <p>Testing...</p>
-      )}
+      <div>
+        <h1>Telemetry Data</h1>
+        <p>Time: {latestTelemetry.Time}</p>
+        <p>Speed: {latestTelemetry.Speed}</p>
+        <p>RPM: {latestTelemetry.RPM}</p>
+      </div>
     </div>
   );
 };
